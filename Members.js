880e707c02d2e182274e86f5b3bd403ede98f5f4
Matt Bianco <matthewbianco@protonmail.com>
2021-11-05 13:56:30 -0400

    added moment.js

---
+++ Members.js
@@ -0,0 +1,22 @@
+const members = [
+    {
+        id: 1,
+        name: 'john',
+        email: 'john@email.com',
+        status: 'active'
+    },
+    {
+        id: 1,
+        name: 'johnny',
+        email: 'johnny@email.com',
+        status: 'inactive'
+    },
+    {
+        id: 1,
+        name: 'tony',
+        email: 'tont@email.com',
+        status: 'active'
+    }
+];
+
+module.exports = members;
--- server.js
+++ server.js
@@ -1,18 +1,32 @@
 const express = require('express');
 const path = require('path');
+const members = require('./Members');
+const moment = require('moment');
 
 const app = express();
+
+const logger = (req, res, next) => {
+
+    // req.protocol returns 'http', req.get('host) gets us the 'localhost:5000', req.originalUrl gets us the '/api/members/ 
+    console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}: ${moment().format()}`);
+    next();
+};
+
+// Init middleware
+ app.use(logger)
+// GETS all members
+app.get('/api/members', (req, res) => res.json(members))
+
+// this creates a relative path for the server. meaning every file in the public directory will now be launched when the server starts. AKA a static folder
+// 
+app.use(express.static(path.join(__dirname, 'public')))
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
 
 // This is used to create a direct path to load a single page to the server. It is inefficent as you would need to make one of these for each page you made
 
 // app.get('/', (req, res) => {
 //     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 // });
-
-// this creates a relative path for the server. meaning every file in the public directory will now be launched when the server starts
-
-app.use(express.static(path.join(__dirname, 'public')))
-
-const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, console.log(`Server started on port ${PORT}`));
